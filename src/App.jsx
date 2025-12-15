5544345a25f67d175b3e6653e507091d6e4f3721
makumasaka <63078396+makumasaka@users.noreply.github.com>
2025-12-14 22:44:01 -0800

    cleaning project for portfolio redesign

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
 import './styles/style.css'
-import { Canvas } from '@react-three/fiber'
-import Experience from './components/Experience'
-import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
-    <>
-      <Canvas
-        className="r3f"
-        camera={{
-          fov: 45,
-          near: 0.1,
-          far: 2000,
-          position: [-3, 1.5, 4]
-        }}
-      >
-        <Experience />
-      </Canvas>
-      <Analytics />
-    </>
+    <div className="app">
+      <div className="card">
+        <p className="message">Mako's portfolio is under construction. Come back soon :)</p>
+      </div>
+    </div>
   )
 }
 
